b04910743c13c1f924a676e2081929608b4684ab
Kate <kate.goncharik.artist@gmail.com>
2024-05-08 15:11:54 +0300

    fix: correct import

--- src/providers/app.tsx
+++ src/providers/app.tsx
@@ -1,7 +1,7 @@
 import type { JSX } from 'react';
 import { RouterProvider } from 'react-router-dom';
 
-import { router } from '@/components/router/router';
+import { router } from '@/routes/router';
 
 export const AppProvider = (): JSX.Element => {
   return <RouterProvider router={router} />;
