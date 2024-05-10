123f9fa36a51ae5363526bc7113fb0866bc9ea5f
Kate Goncharick <113840749+KateGoncharik@users.noreply.github.com>
2024-05-10 15:52:17 +0300

    fix: replace error-boundary with library

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,15 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { ErrorBoundary } from 'react-error-boundary';
 
 import { App } from './app/app';
-import ErrorBoundary from './components/error-boundary/error-boundary';
+import ErrorPage from './pages/error-page';
 
 import '@/assets/styles/main.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ErrorBoundary>
+    <ErrorBoundary FallbackComponent={ErrorPage}>
       <App />
     </ErrorBoundary>
   </StrictMode>,
