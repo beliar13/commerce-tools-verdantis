dbee7da4342678a5be3486d7eb40efa5ea3d3231
Mideli <muzipovanastia@gmail.com>
2024-05-16 14:07:25 +0300

    feat: add validation for password

--- src/features/login-form/login-form.schema.ts
+++ src/features/login-form/login-form.schema.ts
@@ -1,8 +1,14 @@
 import { z } from 'zod';
 
 export const loginSchema = z.object({
-  email: z.string().min(3),
-  password: z.string().min(3),
+  email: z.string(),
+  password: z
+    .string()
+    .min(8, 'Password must be at least 8 characters long')
+    .regex(/[A-Z]+/, 'Password must contain at least one uppercase letter')
+    .regex(/[a-z]+/, 'Password must contain at least one lowercase letter')
+    .regex(/[0-9]+/, 'Password must contain at least one lowercase letter')
+    .regex(/^\S+.\S+$/, 'Password must not contain leading or trailing whitespace.'),
 });
 
 export type LoginInfo = z.infer<typeof loginSchema>;
