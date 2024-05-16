e45055f70543ce610c8c4d36ff65dcb87931cbbf
Mideli <muzipovanastia@gmail.com>
2024-05-16 15:05:12 +0300

    refactor: change error msg

--- src/features/login-form/login-form.schema.ts
+++ src/features/login-form/login-form.schema.ts
@@ -7,7 +7,7 @@ export const loginSchema = z.object({
     .min(8, 'Password must be at least 8 characters long')
     .regex(/[A-Z]+/, 'Password must contain at least one uppercase letter')
     .regex(/[a-z]+/, 'Password must contain at least one lowercase letter')
-    .regex(/[0-9]+/, 'Password must contain at least one lowercase letter')
+    .regex(/[0-9]+/, 'Password must contain at least one number')
     .regex(/^\S+.+\S+$/, 'Password must not contain leading or trailing whitespace.'),
 });
 
