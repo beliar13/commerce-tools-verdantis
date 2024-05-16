d7bf1364260f43de66a3fa5b2263ea5d42ffaac0
Mideli <muzipovanastia@gmail.com>
2024-05-16 14:53:45 +0300

    feat: add valiation for email

--- src/features/login-form/login-form.schema.ts
+++ src/features/login-form/login-form.schema.ts
@@ -1,14 +1,14 @@
 import { z } from 'zod';
 
 export const loginSchema = z.object({
-  email: z.string(),
+  email: z.string().email('Email address must be properly formatted'),
   password: z
     .string()
     .min(8, 'Password must be at least 8 characters long')
     .regex(/[A-Z]+/, 'Password must contain at least one uppercase letter')
     .regex(/[a-z]+/, 'Password must contain at least one lowercase letter')
     .regex(/[0-9]+/, 'Password must contain at least one lowercase letter')
-    .regex(/^\S+.\S+$/, 'Password must not contain leading or trailing whitespace.'),
+    .regex(/^\S+.+\S+$/, 'Password must not contain leading or trailing whitespace.'),
 });
 
 export type LoginInfo = z.infer<typeof loginSchema>;
