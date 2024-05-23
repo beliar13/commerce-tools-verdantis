af1b826d0eeffe9385a34bf8eded875d9efb334b
beliar13 <132068920+beliar13@users.noreply.github.com>
2024-05-23 11:54:16 +0300

    feat: add burger menu, collapse on less then 800px, modify test for header for now

--- src/components/header/header.test.tsx
+++ src/components/header/header.test.tsx
@@ -1,20 +1,25 @@
 import { MemoryRouter } from 'react-router-dom';
 
 import { render, screen } from '@testing-library/react';
+import { userEvent as userAction } from '@testing-library/user-event';
 import { describe, expect, it } from 'vitest';
 
 import { Header } from '.';
 
 describe('Header', () => {
-  it('should render', () => {
+  it('should contain menu and register and login buttons', async () => {
     render(
       <MemoryRouter>
         <Header />
       </MemoryRouter>,
     );
 
-    const loginLink = screen.getByText('login');
-    const registrationLink = screen.getByText('registration');
+    const menu = screen.getByRole('button', { name: /menu/i });
+    await userAction.click(menu);
+    await screen.findByText(/login/i);
+
+    const loginLink = screen.getByText(/login/i);
+    const registrationLink = screen.getByText(/registration/i);
 
     expect(loginLink).toBeInTheDocument();
     expect(registrationLink).toBeInTheDocument();
