4271e349db658675eb7259e9a3f359807cd9406e
Mideli <muzipovanastia@gmail.com>
2024-05-25 15:35:40 +0300

    feat: add address card

---
+++ src/components/profile/address-card/adress-card.tsx
@@ -0,0 +1,32 @@
+import type { FC } from 'react';
+
+import { Box, List, ListItem, ListItemText } from '@mui/material';
+
+import { RegistrationAddress } from '@/features/registration-form/registration-form-types';
+
+export const AddressCard: FC<{ addressInfo: RegistrationAddress; isDefault: boolean }> = ({
+  addressInfo,
+  isDefault,
+}) => {
+  const { city, country, postalCode, streetName } = addressInfo;
+  return (
+    <Box
+      className={`${isDefault ? 'bg-green-50' : 'bg-gray-50'} w-fit rounded-md border border-solid  border-[#6fa45e]`}
+    >
+      <List>
+        <ListItem>
+          <ListItemText primary={`Country: ${country}`} />
+        </ListItem>
+        <ListItem>
+          <ListItemText primary={`City: ${city}`} />
+        </ListItem>
+        <ListItem>
+          <ListItemText primary={`Street: ${streetName}`} />
+        </ListItem>
+        <ListItem>
+          <ListItemText primary={`Postal code: ${postalCode}`} />
+        </ListItem>
+      </List>
+    </Box>
+  );
+};
