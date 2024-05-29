2053606351929bd0dce9bec8b658916e623717e7
beliar13 <132068920+beliar13@users.noreply.github.com>
2024-05-29 22:50:15 +0300

    feat: add product page with name, desc, images with slider, route

---
+++ src/pages/product-page.constants.ts
@@ -0,0 +1,26 @@
+export const sliderSettings = {
+  dots: true,
+  infinite: true,
+  slidesToScroll: 1,
+  slidesToShow: 1,
+  speed: 500,
+};
+
+export const descStyles = {
+  color: 'rgba(238, 238,238, 0.87)',
+  marginTop: 2,
+  padding: 3,
+};
+
+export const titleStyles = {
+  color: 'rgba(238, 238,238, 0.87)',
+};
+
+export const boxStyles = {
+  cursor: 'pointer',
+};
+
+export const imgStyles = {
+  height: 'auto',
+  width: '100%',
+};
