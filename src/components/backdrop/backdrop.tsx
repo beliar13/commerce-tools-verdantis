be3d66fe98d201b466d52fa577979fbd81135366
beliar13 <132068920+beliar13@users.noreply.github.com>
2024-06-06 10:18:19 +0300

    feat: add test for backdrop component

--- src/components/backdrop/backdrop.tsx
+++ src/components/backdrop/backdrop.tsx
@@ -4,8 +4,8 @@ import { Backdrop, CircularProgress } from '@mui/material';
 
 export const LoadingBackdrop: FC<{ open: boolean }> = ({ open }) => {
   return (
-    <Backdrop open={open} sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
-      <CircularProgress />;
+    <Backdrop data-testid="backdrop" open={open} sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
+      <CircularProgress data-testid="circular-progress" />;
     </Backdrop>
   );
 };
