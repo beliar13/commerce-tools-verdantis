1a3a02a978e3c969041e331d01cd3b3d6aa20fe4
beliar13 <132068920+beliar13@users.noreply.github.com>
2024-06-08 21:31:00 +0300

    feat: add about page, info about team members

---
+++ src/pages/about-page.constants.ts
@@ -0,0 +1,34 @@
+import { Members } from './about-page.types';
+
+export const teamMembers: Members = [
+  {
+    bio: 'Kate has extensive experience in leading development teams and driving project success. Like dancing as well as cats.',
+    collaboration: 'Oversaw the entire project lifecycle, ensuring alignment with goals and objectives.',
+    contributions: 'Provided guidance and support to team members. Implement catalog page.',
+    github: 'https://github.com/KateGoncharik',
+    name: 'Kate Goncharik',
+    nickname: 'KateGoncharik',
+    photo: 'src/assets/photo/kate.webp',
+    role: 'team leader',
+  },
+  {
+    bio: 'Anastasiia is a highly skilled developer with the passion for creating robust and scalable apps.',
+    collaboration: 'Developed the core architecture of project, focusing on performance and scalability.',
+    contributions: 'Implement authentication and user management.',
+    github: 'https://github.com/mideli37',
+    name: 'Anastasiia Muzipova',
+    nickname: 'mideli37',
+    photo: 'url',
+    role: 'lead developer',
+  },
+  {
+    bio: 'Den is dedicated developer who excels in writing clean and efficient code.',
+    collaboration: 'Implement product',
+    contributions: 'Contributed to the development of key features, enhancing functionality and user experience.',
+    github: 'https://github.com/beliar13',
+    name: 'Denis Sevostyanov',
+    nickname: 'beliar13',
+    photo: 'src/assets/photo/den.jpg',
+    role: 'developer',
+  },
+];
