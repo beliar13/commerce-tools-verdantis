0e084d4d3e947290cdebb9358fdb29faa8222be6
Kate <kate.goncharik.artist@gmail.com>
2024-06-12 14:14:41 +0300

    feat: add confirm

--- src/features/cart/clear-cart/clear-cart.tsx
+++ src/features/cart/clear-cart/clear-cart.tsx
@@ -1,46 +1,87 @@
-import { MutableRefObject } from 'react';
+import { useState } from 'react';
+import { type FC, MutableRefObject } from 'react';
 
-import { Button } from '@mui/material';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
 
-import { removeLineItemFromCart } from '@/lib/axios/requests/remove-line-item-from-cart';
 import { CartResponse } from '@/lib/axios/requests/schemas/cart-schema';
+import { LineItem } from '@/lib/axios/requests/schemas/line-item-schema';
 import { useCartStore } from '@/stores/cart-store';
 import { useTokenStore } from '@/stores/token-store';
 
-type SetterForCartRef = MutableRefObject<(cart: CartResponse) => void>;
+import { handleRemoveProduct } from './handle-remove-product';
+
 type ClearCartData = {
   cart: CartResponse;
   setterForCartRef: SetterForCartRef;
   token: string;
 };
 
-const handleClearCart = ({ cart, setterForCartRef, token }: ClearCartData): void => {
-  const lineItems = cart.lineItems;
-  lineItems.forEach((lineItem) => {
-    handleRemoveProduct(token, cart, lineItem.id, setterForCartRef);
-  });
-};
+export type SetterForCartRef = MutableRefObject<(cart: CartResponse) => void>;
 
-const handleRemoveProduct = (
-  token: string,
-  cart: CartResponse,
-  lineItemId: string,
-  setterForCartRef: MutableRefObject<(cart: CartResponse) => void>,
-): void => {
-  const setCart = setterForCartRef.current;
-  removeLineItemFromCart(token, cart.id, lineItemId, cart.version).then(
-    (res) => {
-      setCart(res);
-    },
-    (err) => console.error(err),
-  );
-};
-
-export const ClearCart = ({ setterForCartRef }: { setterForCartRef: SetterForCartRef }): JSX.Element => {
+export const ClearCart: FC<{ setterForCartRef: SetterForCartRef }> = ({ setterForCartRef }) => {
   const { cart } = useCartStore();
   const { token } = useTokenStore();
-  if (!token || !cart) {
-    throw new Error('Token and cart expected');
-  }
-  return <Button onClick={() => handleClearCart({ cart, setterForCartRef, token })}>clear cart</Button>;
+
+  const [open, setOpen] = useState(false);
+
+  const handleClickOpen = (): void => {
+    setOpen(true);
+  };
+
+  const handleCancel = (): void => {
+    setOpen(false);
+  };
+
+  const handleClearCart = ({ cart, setterForCartRef, token }: ClearCartData): void => {
+    if (!token || !cart) {
+      throw new Error('Token and cart expected');
+    }
+    const lineItems = cart.lineItems;
+    lineItems.forEach((lineItem: LineItem) => {
+      handleRemoveProduct(token, cart, lineItem.id, setterForCartRef);
+    });
+    setOpen(false);
+  };
+
+  return (
+    <>
+      <Button onClick={handleClickOpen} variant="outlined">
+        Open alert dialog
+      </Button>
+      <Dialog
+        aria-describedby="alert-dialog-description"
+        aria-labelledby="alert-dialog-title"
+        onClose={handleCancel}
+        open={open}
+      >
+        <DialogTitle id="alert-dialog-title" sx={{ backgroundColor: 'primary.light' }}>
+          {'Clear cart'}
+        </DialogTitle>
+        <DialogContent sx={{ backgroundColor: 'primary.light' }}>
+          <DialogContentText id="alert-dialog-description">Are you sure?</DialogContentText>
+        </DialogContent>
+        <DialogActions sx={{ backgroundColor: 'primary.main' }}>
+          <Button onClick={handleCancel} sx={{ color: 'primary.contrastText' }}>
+            Cancel
+          </Button>
+          <Button
+            onClick={() => {
+              if (!token || !cart) {
+                throw new Error('Token and cart expected');
+              }
+              handleClearCart({ cart, setterForCartRef, token });
+            }}
+            sx={{ color: 'primary.contrastText' }}
+          >
+            Yes
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </>
+  );
 };
