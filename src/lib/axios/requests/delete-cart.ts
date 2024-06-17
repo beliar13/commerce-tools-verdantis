a1b52b1c2f936bb479ceab24b81a26ed5f6c1f1d
Kate Goncharick <113840749+KateGoncharik@users.noreply.github.com>
2024-06-17 23:13:09 +0300

    fix: correct naming

--- src/lib/axios/requests/delete-cart.ts
+++ src/lib/axios/requests/delete-cart.ts
@@ -10,12 +10,12 @@ export async function deleteCart(BEARER_TOKEN: string, cartId: string, cartVersi
   const query = `/${envVariables.PROJECT_KEY}/me/carts/${cartId}?version=${cartVersion}`;
   const encoded = encodeURI(query);
   try {
-    const createCartResult = await apiInstance.delete(encoded, {
+    const deleteCartResult = await apiInstance.delete(encoded, {
       headers: {
         Authorization: `Bearer ${BEARER_TOKEN}`,
       },
     });
-    return cartResponseSchema.parse(createCartResult.data);
+    return cartResponseSchema.parse(deleteCartResult.data);
   } catch (e) {
     console.error('Error occurred while deleting cart:', e);
     if (isAxiosError(e)) {
