016df624fea06a0eb560d936c28c56b9339fb539
Kate Goncharick <113840749+KateGoncharik@users.noreply.github.com>
2024-06-28 13:18:38 +0300

    feat: add favicon image on main

--- src/pages/main-page.tsx
+++ src/pages/main-page.tsx
@@ -1,8 +1,9 @@
 import { FC } from 'react';
 import { Link as RouterLink } from 'react-router-dom';
 
-import { Button, Stack, Typography } from '@mui/material';
+import { Button, Icon, Stack, Typography } from '@mui/material';
 
+import favicon from '@/assets/img/favicon.png';
 import { PromoCodesWrapper } from '@/features/main-content/promo-codes-wrapper';
 
 const joinButtonStyles = {
@@ -10,15 +11,37 @@ const joinButtonStyles = {
   width: { lg: '30%', md: '40%', sm: '55%', xs: '75%' },
 };
 
+const mainLogoStyles = { lg: '40%', md: '50%', sm: '70%', xs: '90%' };
+
 const MainPage: FC = () => {
   return (
     <Stack className="flex-col items-center justify-center p-3">
+      <Stack
+        className="flex items-center justify-center gap-1 "
+        sx={{ flexDirection: { lg: 'row-reverse', md: 'column', sm: 'column', xs: 'column' } }}
+      >
+        <Typography
+          sx={{ fontSize: { lg: '2.5em', md: '2em', sm: '1.7em', xs: '1.5em' }, textAlign: 'center', transition: '2s' }}
+        >
+          Welcome to Verdantis flowers shop
+        </Typography>
+
+        <Icon
+          alt="main-logo"
+          component="img"
+          src={favicon}
+          sx={{
+            height: mainLogoStyles,
+            width: mainLogoStyles,
+          }}
+        />
+      </Stack>
       <PromoCodesWrapper />
       <Stack
-        className="flex-col items-center justify-center gap-1 p-4"
+        className="flex-col items-center justify-center gap-1 px-4 py-6"
         sx={{ backgroundColor: 'secondary.main', width: '100%' }}
       >
-        <Typography className="text-center" sx={{ fontSize: '2em' }}>
+        <Typography className="text-center" sx={{ fontSize: '1.5em' }}>
           Would like to join us?
         </Typography>
         <Button component={RouterLink} sx={joinButtonStyles} to="/login" variant="outlined">
