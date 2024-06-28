c48d25d33c41445aca34c7a5b68a27a8485b9265
Kate Goncharick <113840749+KateGoncharik@users.noreply.github.com>
2024-06-28 12:48:03 +0300

    fix: correct main-navigation

--- src/pages/main-page.tsx
+++ src/pages/main-page.tsx
@@ -1,23 +1,34 @@
-import { FC, ReactNode } from 'react';
+import { FC } from 'react';
 import { Link as RouterLink } from 'react-router-dom';
 
-import { Button } from '@mui/material';
+import { Button, Stack, Typography } from '@mui/material';
 
 import { PromoCodesWrapper } from '@/features/main-content/promo-codes-wrapper';
 
-const MainPage: FC<{
-  children?: ReactNode;
-}> = () => {
+const joinButtonStyles = {
+  transition: '2s',
+  width: { lg: '30%', md: '40%', sm: '55%', xs: '75%' },
+};
+
+const MainPage: FC = () => {
   return (
-    <>
-      <Button component={RouterLink} to="/login">
-        login
-      </Button>
-      <Button component={RouterLink} to="/registration">
-        registration
-      </Button>
+    <Stack className="flex-col items-center justify-center p-3">
       <PromoCodesWrapper />
-    </>
+      <Stack
+        className="flex-col items-center justify-center gap-1 p-4"
+        sx={{ backgroundColor: 'secondary.main', width: '100%' }}
+      >
+        <Typography className="text-center" sx={{ fontSize: '2em' }}>
+          Would like to join us?
+        </Typography>
+        <Button component={RouterLink} sx={joinButtonStyles} to="/login" variant="outlined">
+          login
+        </Button>
+        <Button component={RouterLink} sx={joinButtonStyles} to="/registration" variant="outlined">
+          registration
+        </Button>
+      </Stack>
+    </Stack>
   );
 };
 
